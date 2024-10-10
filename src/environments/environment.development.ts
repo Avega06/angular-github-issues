e7de6bf5fcadb75169f0c7837d3b7098f00e3520
Alex Vega <alexvega06.1996@gmail.com>
2024-10-10 17:25:11 -0300

    Add new key

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseUrl: 'https://api.github.com/repos/angular/angular',
   gitHubToken:
-    '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',
+    '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',
 };
