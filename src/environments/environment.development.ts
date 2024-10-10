731dd72546ff5fce7e44cb2c21417aff64233dfa
Alex Vega <alexvega06.1996@gmail.com>
2024-10-10 17:53:15 -0300

    Fix envs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   baseUrl: 'https://api.github.com/repos/angular/angular',
-  gitHubToken:
-    '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',
+  githubToken: process.env['GITHUB_TOKEN'],
 };
