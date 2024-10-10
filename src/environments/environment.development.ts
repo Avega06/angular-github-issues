6ece171311177b7457f041b9dfdee2f461a94fe4
Alex Vega <alexvega06.1996@gmail.com>
2024-10-10 17:11:23 -0300

    Fixed secret

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseUrl: 'https://api.github.com/repos/angular/angular',
   gitHubToken:
-    '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',
+    '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',
 };
